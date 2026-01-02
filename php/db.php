02fb212038b249f16d52085f5bac4ca60b726490
Luis Josue Torres Lucas <jolsata24@gmail.com>
2026-01-01 21:38:31 -0500

    nuevo

--- php/db.php
+++ php/db.php
@@ -1,8 +1,6 @@
 <?php
-// db.php
-// Ajusta estas variables a tu servidor
 $DB_HOST = 'localhost';
-$DB_NAME = 'milegajo';   // ejemplo: dremh_legajos
+$DB_NAME = 'milegajo'; 
 $DB_USER = 'root';
 $DB_PASS = '123456';
 $DB_CHAR = 'utf8mb4';
@@ -12,7 +10,6 @@ $options = [
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES => false,
 ];
-
 try {
     $pdo = new PDO(
         "mysql:host={$DB_HOST};dbname={$DB_NAME};charset={$DB_CHAR}",
@@ -21,6 +18,5 @@ try {
         $options
     );
 } catch (PDOException $e) {
-    // En desarrollo puedes mostrar el error; en producción loguealo y muestra un mensaje genérico
     die("Error al conectar a la base de datos: " . $e->getMessage());
 }
